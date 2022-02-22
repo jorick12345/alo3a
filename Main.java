056a760f28be6a65b5099e8caf0fc6060688c52b
davidcoffa <david.coffa@student.hu.nl>
2022-02-22 14:53:00 +0100

    Test

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[]args) {
+        ArrayList voetbalclub  = new ArrayList();
+        voetbalclub.add("Ado");voetbalclub.add("Ajax");voetbalclub.add("Feyenoord");
+        System.out.println();
+    }
+}
